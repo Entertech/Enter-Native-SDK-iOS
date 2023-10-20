2bf36771fdef865deee0e4bbaf448d00f103b18f
LK-LINK <ke.liful@gmail.com>
2023-10-20 17:27:14 +0800

    update： 添加文档

--- README.md
+++ README.md
@@ -1,2 +1,284 @@
-# Enter-Native-SDK-iOS
-Native Affective Algorithm For iOS
+# 回车科技iOS情感计算本地算法SDK
+
+回车情感计算根据回车产品采集的脑电、心率以及压电压阻进行数据分析，同时能给出包括：放松度、注意力、愉悦值，压力值、激动度（内测）在内的多种情绪情感值。详情请查看[官网](https://www.entertech.cn/)。
+
+在开始开发前，请先查看回车[开发文档](https://docs.affectivecloud.com/)，了解情感计算算法所能提供的服务具体说明，确定好你的应用中所需要的服务。你还需要联系管理员获取算法授权文件，然后再进行开发。
+
+为了方便你进行 iOS 平台的快速开发，我们提供了情感云快速开发 SDK，通过本 SDK 你可以快速地将情感云的能力集成到你的 app 中。
+
+## 版本需求
+- iOS 14 及以上（不支持模拟器）
+- Xcode 13 及以上
+- Swift 5.5 及以上
+
+## 安装
+### Swift Package Manager
+
+Package添加下面这行到你的 Package.swift：
+```
+.package(name: "Enter-Native-SDK-iOS", url: "https://github.com/Entertech/Enter-Native-SDK-iOS.git", from: "1.0.0")
+```
+
+Project添加下面的链接到你的 Package Dependencies 中：
+```
+https://github.com/Entertech/Enter-Native-SDK-iOS.git
+```
+
+## 使用
+
+### 头文件引用
+XCODE 13版本及以上创建的工程可以直接添加以下头文件：
+```
+import AffectiveAlgorithmSDK_iOS
+```
+早期版本工程可以在Bridge-Header.h中添加以下头文件：
+```
+#import <AffectiveAlgorithmSDK_iOS/AffectiveWrapper.h>
+```
+
+### 接口头文件查看
+所有用到的接口和封装对象都在[AffectiveWrapper.h](https://github.com/Entertech/Enter-Native-SDK-iOS/blob/main/Sources/AffectiveAlgorithmSDK_iOS.xcframework/ios-arm64/AffectiveAlgorithmSDK_iOS.framework/Headers/AffectiveWrapper.h)中。
+
+### 初始化SDK
+`AffectiveHandler` 是整个 SDK 的入口类，你需要先初始化 `AffectiveHandler`。
+```swift
+class Demo {
+    var handler: AffectiveHandler?
+    
+    init {
+        handler = AffectiveHandler()
+    }
+    
+    ......
+}
+
+```
+
+### 设置授权信息
+如果你获得了回车科技的授权，会得到一个授权文件`ENTERTECH_NATIVE_SDK_LICENSE.txt`，你需要将该文件加入工程，然后在初始化 SDK 之前设置授权信息：
+```swift
+    // 获取授权文件路径
+    guard let path = Bundle.main.path(forResource: "ENTERTECH_NATIVE_SDK_LICENSE", ofType: "txt") else {
+        return false
+    }
+    // 验证授权
+    guard let auth = handler?.auth(fromBundle: path), auth else {
+        //验证失败
+        return false
+    }
+
+```
+
+#### 授权失败
+- 无法找到path
+    - 如果加入了文件还是无法找到path，请检查是否添加了`Copy items if needed`和`Add to targets`。
+- auth 返回false
+    - 确认授权文件是否有改动。
+    
+### 实时数据计算
+将蓝牙数据的回调`Data`类型数据传入，关于蓝牙部分请参考[回车蓝牙SDK](https://github.com/Entertech/Enter-Biomodule-BLE-iOS-SDK.git)
+
+#### 数据转化
+因为算法用Objective-C封装，需要对蓝牙传过来的`Data`类型转化才可使用，此处data为蓝牙回调数据。
+```swift
+    // 心率转化, 心率只有首个字节是有效数据
+    let mutableArray = NSMutableArray()
+    if let hr = data.first {
+        mutableArray.add(NSNumber(value: hr))
+    }
+    
+    // 脑波，压电压阻转化
+    let mutableArray = NSMutableArray()
+    data.forEach { byte in
+        mutableArray.add(NSNumber(value: byte))
+    }
+}
+
+```
+
+#### 心率计算
+心率累计3组数据数据进行一次计算并返回结果，不计算时返回nil。
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let hrEnable = handler?.getHREnable(), hrEnable else {
+        return
+    }
+    if let result = handler?.appendHR(mutableArray) {
+        // 此处对 result 进行处理, result 类型`HRAffectiveResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 双通道脑电计算
+双通道脑电累计50组数据进行一次计算并返回结果，不计算时返回nil。
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let eegEnable = handler?.getEEGEnable(), eegEnable else {
+        return
+    }
+    if let result = handler?.appendEEG(mutableArray) {
+        // 此处对 result 进行处理, result 类型`EEGAffectiveResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 单通道脑电计算
+单通道脑电累计30组数据进行一次计算并返回结果，不计算时返回nil。
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let sceegEnable = handler?.getSCEEGEnable(), sceegEnable else {
+        return
+    }
+    if let result = handler?.appendSCEEG(mutableArray) {
+        // 此处对 result 进行处理, result 类型`SCEEGAffectiveResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 压电压阻计算
+压电压阻累计15组数据进行一次计算并返回结果，不计算时返回nil。
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let peprEnable = handler?.getPEPREnable(), peprEnable else {
+        return
+    }
+    if let result = handler?.appendPEPR(mutableArray) {
+        // 此处对 result 进行处理, result 类型`PEPRAffectiveResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+### 报表获取
+完成数据计算后可以获取报表，不进行计算直接获取报表，会得到空报表。
+
+#### 心率报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let hrEnable = handler?.getHREnable(), hrEnable else {
+        return
+    }
+    if let report = handler?.getHRReport() {
+        // 此处对 report 进行处理, report 类型`HRReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 双通道脑电报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let eegEnable = handler?.getEEGEnable(), eegEnable else {
+        return
+    }
+    if let report = handler?.getEEGReport() {
+        // 此处对 report 进行处理, report 类型`EEGReprotResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 单通道脑电报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let sceegEnable = handler?.getSCEEGEnable(), sceegEnable else {
+        return
+    }
+    if let report = handler?.getSCEEGReport() {
+        // 此处对 report 进行处理, report 类型`SCEEGReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 压电压阻报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let peprEnable = handler?.getPEPREnable(), peprEnable else {
+        return
+    }
+    if let report = handler?.getPEPRReport() {
+        // 此处对 report 进行处理, report 类型`PEPRReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 睡眠报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let sleepEnable = handler?.getSleepEnable(), sleepEnable else {
+        return
+    }
+    if let report = handler?.getSleepReport() {
+        // 此处对 report 进行处理, report 类型`SleepReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 放松度报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let relaxEnable = handler?.getRelaxationEnable(), relaxEnable else {
+        return
+    }
+    if let report = handler?.getRelaxationReport() {
+        // 此处对 report 进行处理, report 类型`RelaxationReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 注意力报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let attentionEnable = handler?.getAttentionEnable(), attentionEnable else {
+        return
+    }
+    if let report = handler?.getAttentionReport() {
+        // 此处对 report 进行处理, report 类型`AttentionReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 压力报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let pressureEnable = handler?.getPressureEnable(), pressureEnable else {
+        return
+    }
+    if let report = handler?.getPressureReport() {
+        // 此处对 report 进行处理, report 类型`PressureReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 心流报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let flowEnable = handler?.getFlowEnable(), flowEnable else {
+        return
+    }
+    if let report = handler?.getFlowReport() {
+        // 此处对 report 进行处理, report 类型`FlowReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 和谐度报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let coherenceEnable = handler?.getCoherenceEnable(), coherenceEnable else {
+        return
+    }
+    if let report = handler?.getCoherenceReport() {
+        // 此处对 report 进行处理, report 类型`CoherenceReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 愉悦度报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let pleasureEnable = handler?.getPleasureEnable(), pleasureEnable else {
+        return
+    }
+    if let report = handler?.getPleasureReport() {
+        // 此处对 report 进行处理, report 类型`PleasureReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+#### 激活度报表
+```swift
+    // 先判断是否可用， 未通过授权是不可用状态
+    guard let arousalEnable = handler?.getArousalEnable(), arousalEnable else {
+        return
+    }
+    if let report = handler?.getArousalReport() {
+        // 此处对 report 进行处理, report 类型`ArousalReportResOC` 在上方`接口头文件查看`可找到查看入口
+    }
+```
+
+
+
+
--- Sources/AffectiveAlgorithmSDK_iOS.xcframework/ios-arm64/AffectiveAlgorithmSDK_iOS.framework/Headers/AffectiveWrapper.h
+++ Sources/AffectiveAlgorithmSDK_iOS.xcframework/ios-arm64/AffectiveAlgorithmSDK_iOS.framework/Headers/AffectiveWrapper.h
@@ -9,181 +9,318 @@
 #define AffectiveWrapper_h
 #import <Foundation/Foundation.h>
 
+/// 激活度报表
 @interface ArousalReportResOC: NSObject
+/// 平均激活度
 @property (nonatomic, assign, readwrite) double arousalAvg;
+/// 激活度全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *arousalRec;
 @end
 
+/// 和谐度报表
 @interface CoherenceReportResOC: NSObject
+/// 平均和谐度
 @property (nonatomic, assign, readwrite) double coherenceAvg;
+/// 和谐时长
 @property (nonatomic, assign, readwrite) int coherenceDuration;
+/// 和谐度列表
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *coherenceRec;
+/// 和谐度全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *coherenceFlag;
 
 @end
 
+/// 愉悦度报表
 @interface PleasureReportResOC: NSObject
+/// 愉悦度均值
 @property (nonatomic, assign, readwrite) double pleasureAvg;
+/// 愉悦度全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *pleasureRec;
 @end
 
+/// 压力报表
 @interface PressureReportResOC: NSObject
+/// 压力均值
 @property (nonatomic, assign, readwrite) double pressureAvg;
+/// 压力全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *pressureRec;
 @end
 
+/// 放松度报表
 @interface RelaxationReportResOC: NSObject
+/// 放松度均值
 @property (nonatomic, assign, readwrite) double relaxationAvg;
+/// 放松度全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *relaxationRec;
 @end
 
+/// 注意力报表
 @interface AttentionReportResOC: NSObject
+/// 注意力均值
 @property (nonatomic, assign, readwrite) double attentionAvg;
+/// 注意力全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *attentionRec;
 @end
 
+/// 睡眠报表
 @interface SleepReportResOC : NSObject
+/// 睡眠全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *sleepCurve;
+/// 睡眠阶段全程记录
+/// WAKE = 0  # 清醒
+/// NREM1 = 1  # 非快速眼动期1（思睡期）
+/// NREM2 = 2  # 非快速眼动期2（浅睡期）
+/// NREM3 = 3  # 非快速眼动期3（深睡期）
+/// REM = 4  # 快速眼动期
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *sleepStage;
+/// 入睡点
 @property (nonatomic) int sleepPoint;
+/// 睡眠潜伏期时间点
 @property (nonatomic) int sleepLatency;
+/// 清醒时长
 @property (nonatomic) int awakeDuration;
+/// REM期时长
 @property (nonatomic) int remDuration;
+/// 浅睡时长
 @property (nonatomic) int lightDuration;
+/// 深睡时长
 @property (nonatomic) int deepDuration;
+/// EEG alpha 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegAlphaCurve;
+/// EEG beta 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegBetaCurve;
+/// EEG theta 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegThetaCurve;
+/// EEG delta 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegDeltaCurve;
+/// EEG gamma 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegGammaCurve;
+/// EEG high beta 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegHighBetaDBCurve;
+/// EEG low beta 全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegLowBetaDBCurve;
+/// EEG 质量全程记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *eegQualityRec;
+/// 体动次数
 @property (nonatomic) int movementCount;
+/// 觉醒次数
 @property (nonatomic) int arousalCount;
+/// 抗干扰能力
 @property (nonatomic) double disturbTolerance;
+/// 体动标志记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *movementRec;
+/// 觉醒标志记录
 @property (strong, nonatomic) NSMutableArray<NSNumber *> *arousalRec;
 @end
 
+/// 心流报表
 @interface FlowReportResOC : NSObject
-@property (nonatomic, assign) double flowAvg; // Average meditation value
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *flowRec; // Full record of meditation
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *flowTipsRec; // Full record of meditation state tips
-@property (nonatomic, assign) double flowPercent; // Percentage of flow state
-@property (nonatomic, assign) double flowDuration; // Duration of flow state
-@property (nonatomic, assign) double flowLatency; // Time to enter the flow state
-@property (nonatomic, assign) int flowCombo; // Maximum continuous time of flow state
-@property (nonatomic, assign) double flowDepth; // Maximum depth of flow state
-@property (nonatomic, assign) int flowBackNum; // Number of flow state recovery tips
-@property (nonatomic, assign) int flowLossNum; // Number of flow state loss tips
+/// 心流平均值
+@property (nonatomic, assign) double flowAvg;
+/// 心流全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *flowRec;
+/// 心流状态提示全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *flowTipsRec;
+/// 心流状态占比
+@property (nonatomic, assign) double flowPercent;
+/// 心流状态时长
+@property (nonatomic, assign) double flowDuration;
+/// 进入心流状态的用时
+@property (nonatomic, assign) double flowLatency;
+/// 心流状态最大连续时间
+@property (nonatomic, assign) int flowCombo;
+/// 心流状态最大深度
+@property (nonatomic, assign) double flowDepth;
+/// 心流状态恢复提示次数
+@property (nonatomic, assign) int flowBackNum;
+/// 心流状态丢失提示次数
+@property (nonatomic, assign) int flowLossNum;
 @end
 
+/// 心率报表
 @interface HRReportResOC: NSObject
+/// 心率全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *hrRec;
+/// 心率变异性全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *hrvRec;
 @end
 
+/// EEG报表
 @interface EEGReprotResOC: NSObject
+/// EEG alpha 全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegAlphaRec;
+/// EEG beta 全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegBetaRec;
+/// EEG theta 全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegThetaRec;
+/// EEG delta 全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegDeltaRec;
+/// EEG gamma 全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegGammaRec;
+/// EEG 质量全程记录
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegQualityRec;
 @end
 
-// SC EEG Report
+// 单通道 EEG 报表
 @interface SCEEGReportResOC : NSObject
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegAlphaRec; // Full course change curve of brain wave α wave energy
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegBetaRec; // Full course change curve of brain wave β wave energy
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegThetaRec; // Full course change curve of brain wave θ wave energy
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegDeltaRec; // Full course change curve of brain wave δ wave energy
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegGammaRec; // Full course change curve of brain wave γ wave energy
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegQualityRec; // Full course change curve of brain wave quality level
+/// 单通道 EEG alpha 全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegAlphaRec;
+/// 单通道 EEG beta 全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegBetaRec;
+/// 单通道 EEG theta 全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegThetaRec;
+/// 单通道 EEG delta 全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegDeltaRec;
+/// 单通道 EEG gamma 全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegGammaRec;
+/// 单通道 EEG 质量全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *eegQualityRec;
 @end
 
+/// 压电压阻报表
 @interface PEPRReportResOC : NSObject
-@property (nonatomic, assign) int hrAvg; // Average heart rate
-@property (nonatomic, assign) int hrMax; // Maximum heart rate
-@property (nonatomic, assign) int hrMin; // Minimum heart rate
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *hrRec; // Full record of heart rate
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *rrRec; // Full record of respiration rate
-@property (nonatomic, assign) int rrAvg; // Average respiration rate
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *hrvRec; // Full record of heart rate variability
+/// 心率均值
+@property (nonatomic, assign) int hrAvg;
+/// 心率区间最大值
+@property (nonatomic, assign) int hrMax;
+/// 心率区间最小值
+@property (nonatomic, assign) int hrMin;
+/// 心率全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *hrRec;
+/// 呼吸率全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *rrRec;
+/// 呼吸率均值
+@property (nonatomic, assign) int rrAvg;
+/// 心率变异性全程记录
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *hrvRec;
+/// 心率变异性均值
 @property (nonatomic, assign) double hrvAvg; // Average heart rate variability
+/// 脉搏波质量(0: NONE, 1: POOR, 2: NORM)
 @property (nonatomic, strong) NSMutableArray<NSNumber *> *bcgQualityRec; // Full record of pulse wave signal quality (0: NONE, 1: POOR, 2: NORM)
+/// 呼吸波质量(0: NONE, 1: POOR, 2: NORM)
 @property (nonatomic, strong) NSMutableArray<NSNumber *> *rwQualityRec; // Full record of respiration wave signal quality (0: NONE, 1: POOR, 2: NORM)
 @end
 
+/// 实时睡眠
 @interface SleepTriggerResOC : NSObject
+/// 睡眠程度
 @property (nonatomic, assign) double sleepDegree; // Degree of sleep
+/// 睡眠状态（0:清醒，1:睡着）
 @property (nonatomic, assign) int sleepState; // Sleep state
 @end
 
+/// 实时心流
 @interface FlowTriggerResOC : NSObject
+/// 心流程度
 @property (nonatomic, assign) double meditation; // Degree of meditation
+/// 心流状态提示（0:初始状态，1:状态恢复提醒， 2:状态丢失提醒）
 @property (nonatomic, assign) double meditationTips; // Meditation state tips
 @end
 
+/// 实时心率
 @interface HRTriggerResOC: NSObject
+/// 心率变异性
 @property (nonatomic, assign, readwrite) double hrv;
+/// 心率
 @property (nonatomic, assign, readwrite) int hr;
 @end
 
+/// 实时 EEG
 @interface EEGTriggerResOC: NSObject
+/// EEG 左脑实时数据
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eeglWave;
+/// EEG 右脑实时数据
 @property (nonatomic, strong, readwrite) NSMutableArray<NSNumber *> *eegrWave;
+/// EEG alpha 实时数据
 @property (nonatomic, assign, readwrite) double eegAlphaPower;
+/// EEG beta 实时数据
 @property (nonatomic, assign, readwrite) double eegBetaPower;
+/// EEG theta 实时数据
 @property (nonatomic, assign, readwrite) double eegThetaPower;
+/// EEG delta 实时数据
 @property (nonatomic, assign, readwrite) double eegDeltaPower;
+/// EEG gamma 实时数据
 @property (nonatomic, assign, readwrite) double eegGammaPower;
+/// EEG 质量实时数据
 @property (nonatomic, assign, readwrite) double eegQuality;
 @end
 
+/// 单通道 EEG 实时数据
 @interface SCEEGTriggerResOC : NSObject
+/// EEG 实时数据
 @property (nonatomic, strong) NSMutableArray<NSNumber *> *eegWave; // Brain wave
+/// EEG alpha 实时数据
 @property (nonatomic, assign) double eegAlphaPower; // Alpha wave energy
+/// EEG beta 实时数据
 @property (nonatomic, assign) double eegBetaPower; // Beta wave energy
+/// EEG theta 实时数据
 @property (nonatomic, assign) double eegThetaPower; // Theta wave energy
+/// EEG delta 实时数据
 @property (nonatomic, assign) double eegDeltaPower; // Delta wave energy
+/// EEG gamma 实时数据
 @property (nonatomic, assign) double eegGammaPower; // Gamma wave energy
+/// EEG 质量实时数据
 @property (nonatomic, assign) double eegQuality; // Quality of the EEG signal
 @end
 
+/// 压电压阻实时数据
 @interface PEPRAffectiveResOC : NSObject
+/// 脉搏波数据
 @property (nonatomic, strong) NSMutableArray<NSNumber *> *bcgWave; // Pulse wave waveform
+/// 呼吸波数据
 @property (nonatomic, strong) NSMutableArray<NSNumber *> *rwWave; // Respiratory wave waveform
+/// 脉搏波质量(0: NONE, 1: POOR, 2: NORM)
 @property (nonatomic, assign) int bcgQuality; // Quality level of pulse wave signal 0: NONE 1: POOR 2: NORM
+/// 呼吸波质量(0: NONE, 1: POOR, 2: NORM)
 @property (nonatomic, assign) int rwQuality; // Quality level of respiratory wave signal 0: NONE 1: POOR 2: NORM
+/// 心率
 @property (nonatomic, assign) int hr; // Heart rate
+/// 心率变异性
 @property (nonatomic, assign) double hrv; // Heart rate variability
+/// 呼吸率
 @property (nonatomic, assign) double rr; // Respiration rate
+/// 呼吸率变异性
 @property (nonatomic, assign) double pressure; // Pressure level
+/// 和谐度变异性
 @property (nonatomic, assign) double coherence; // Coherence
 @end
 
+/// 单通道实时数据综合
 @interface SCEEGAffectiveResOC : NSObject
+/// 放松度实时数据
 @property (nonatomic, assign) double relaxation;
+/// 睡眠实时数据
 @property (nonatomic, strong) SleepTriggerResOC *sleep;
+/// 脑波实时数据
 @property (nonatomic, strong) SCEEGTriggerResOC *eeg;
 @end
 
+/// 双通道实时数据综合
 @interface EEGAffectiveResOC: NSObject
+/// EEG 实时数据
 @property (nonatomic, strong, readwrite) EEGTriggerResOC *eeg;
+/// 睡眠实时数据
 @property (nonatomic, strong, readwrite) SleepTriggerResOC *sleep;
+/// 心流实时数据
 @property (nonatomic, strong, readwrite) FlowTriggerResOC *flow;
+/// 放松度实时数据
 @property (nonatomic, assign, readwrite) double relaxation;
+/// 愉悦度实时数据
 @property (nonatomic, assign, readwrite) double pleasure;
+/// 注意力实时数据
 @property (nonatomic, assign, readwrite) double attention;
 
 @end
 
+/// 心率实时数据综合
 @interface HRAffectiveResOC: NSObject
+/// 心率实时数据
 @property (nonatomic, strong, readwrite) HRTriggerResOC *hr;
+/// 压力值实时数据
 @property (nonatomic, assign, readwrite) double pressure;
+/// 和谐度实时数据
 @property (nonatomic, assign, readwrite) double coherence;
+/// 激活度实时数据
 @property (nonatomic, assign, readwrite) double arousal;
 @end
 
@@ -191,37 +328,83 @@
 @end
 
 @interface AffectiveHandler : NSObject
+/// 初始号
 - (instancetype)init;
-- (void)dealloc;
 
+/// 计算EEG实时
 - (EEGAffectiveResOC *) appendEEG: (NSMutableArray<NSNumber *> *)eegRaw;
+
+/// 计算心率
 - (HRAffectiveResOC *) appendHR: (NSMutableArray<NSNumber *> *)hrRaw;
+
+/// 计算压电压阻
 - (PEPRAffectiveResOC *) appendPEPR: (NSMutableArray<NSNumber *> *)peprRaw;
+
+/// 计算单通道EEG
 - (SCEEGAffectiveResOC *) appendSCEEG: (NSMutableArray<NSNumber *> *)sceegRaw;
+
+/// 获取激活度报表
 - (ArousalReportResOC *) getArousalReport;
+
+/// 获取和谐度报表
 - (CoherenceReportResOC *) getCoherenceReport;
+
+/// 获取愉悦度报表
 - (PleasureReportResOC *) getPleasureReport;
+
+/// 获取压力报表
 - (PressureReportResOC *) getPressureReport;
+
+/// 获取放松度报表
 - (RelaxationReportResOC *) getRelaxationReport;
+
+/// 获取注意力报表
 - (AttentionReportResOC *) getAttentionReport;
+
+/// 获取睡眠报表
 - (SleepReportResOC *) getSleepReport;
+
+/// 获取心率报表
 - (HRReportResOC *) getHRReport;
+
+/// 获取心率报表
 - (EEGReprotResOC *) getEEGReport;
+
+/// 获取压电压阻报表
 - (PEPRReportResOC *) getPEPRReport;
+
+/// 获取单通道报表
 - (SCEEGReportResOC *) getSCEEGReport;
+
+/// 获取心流报表
 - (FlowReportResOC *) getFlowReport;
+
+/// 获取授权
 - (BOOL)AuthFromBundle: (NSString *) path;
+
+/// 获取激活度是否可用
 - (BOOL) getArousalEnable;
+/// 获取和谐度是否可用
 - (BOOL) getCoherenceEnable;
+/// 获取愉悦度是否可用
 - (BOOL) getPleasureEnable;
+/// 获取压力是否可用
 - (BOOL) getPressureEnable;
+/// 获取放松度是否可用
 - (BOOL) getRelaxationEnable;
+/// 获取注意力是否可用
 - (BOOL) getAttentionEnable;
+/// 获取睡眠是否可用
 - (BOOL) getSleepEnable;
+/// 获取心流是否可用
 - (BOOL) getFlowEnable;
+/// 获取双通道脑电是否可用
 - (BOOL) getEEGEnable;
+/// 获取心率是否可用
 - (BOOL) getHREnable;
+/// 获取单通道脑电是否可用
 - (BOOL) getSCEEGEnable;
+/// 获取压电压阻是否可用
 - (BOOL) getPEPREnable;
 
 @end
